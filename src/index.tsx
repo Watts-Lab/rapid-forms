9dc3431966f8d4a5d56f5ad50701bd11b17ac5cc
Sumant R Shringari <sumant.r.shringari@gmail.com>
2022-04-18 07:27:39 -0400

    the first commit

---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+// components/survey/index.tsx
+import React from 'react';
+import Checkbox from './inputs/checkbox';
+import Radio from './inputs/radio';
+import Submit from './inputs/submit';
+
+export type Element = {
+  type: string;
+  name: string;
+  title: string;
+  description?: any;
+  choices?: string[];
+};
+
+export type Attribute = {
+  key: string;
+  value: string;
+};
+
+export interface FormProps {
+  elements: Element[];
+  hiddenAttributes?: Attribute[];
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export const SinglePageForm = ({
+  elements,
+  hiddenAttributes,
+  onChange,
+  onSubmit,
+}: FormProps) => {
+  const createInput = (element: Element) => {
+    switch (element.type) {
+      case 'radio':
+        return <Radio {...element} onChange={onChange} />;
+      case 'checkbox':
+        return <Checkbox {...element} onChange={onChange} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} data-testid="watts-form">
+      {elements.map(element => createInput(element))}
+      {hiddenAttributes?.map(attribute => (
+        <input type="hidden" name={attribute.key} value={attribute.value} />
+      ))}
+      <Submit />
+    </form>
+  );
+};
+
+export default SinglePageForm;
---
+++ src/inputs/submit.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function Submit() {
+  return (
+    <input
+      type="submit"
+      className="form-input w-full"
+      data-testid="watts-form-submit"
+    />
+  );
+}
